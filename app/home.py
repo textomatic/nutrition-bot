7dcb64ef9afb5e1509bd5c3cdabed2645c9efdec
textomatic <102237745+textomatic@users.noreply.github.com>
2023-03-21 00:55:53 -0400

    Update Streamlit page

--- app/home.py
+++ app/home.py
@@ -8,31 +8,37 @@ def main():
     st.markdown('Ask any nutrition-related question and you\'ll get answers from both scientific research papers and social media')
 
     with st.form(key='my_form'):
-        query = st.text_input('Enter your query:')
-        submit = st.form_submit_button('Ask!')
+        query = st.text_input('Enter your query:') # Input text box
+        submit = st.form_submit_button('Ask!') # Submit button
 
     if submit and query is not None:
 
-        response = requests.get('http://35.196.123.58:8060/search/'+query)
-        response_json = response.json()
+        response = requests.get('http://35.207.46.114:8060/search/'+query) # API endpoint for nutritionbot
+        response_json = response.json() # Get json from response
+        ans_respp = response_json['research_paper_results']
+        ans_socmed = response_json['reddit_results']
 
-        respp, socmed = st.columns(2)
+        respp, socmed = st.columns([2, 1])
 
-        with respp:
-            st.subheader("Research Papers")
-            # respp_ans = ''
-            for ans in response_json:
+        with respp: # One column for answers from research paper
+            st.subheader("Research Papers") 
+            for ans in ans_respp:
                 ans_index_start = ans['content'].find(ans['answer'])
                 ans_index_end = ans_index_start + len(ans['answer'])
-                st.markdown(ans['content'][:ans_index_start] + "`" + ans['content'][ans_index_start:ans_index_end] + "`" + ans['content'][ans_index_end:])
-                # st.write(ans['content'])
-                st.write(f'Score: {ans["score_y"]:.3f}')
-                # respp_ans += ans['content']
-                # respp_ans += f'Score: {ans["score_y"]:.3f}\n\n'
-            # st.write(respp_ans)
+                st.markdown(ans['content'][:ans_index_start] + "**<span style='color:#02f53f'>" + ans['content'][ans_index_start:ans_index_end] + "</span>**" + ans['content'][ans_index_end:], unsafe_allow_html=True) # Answer highlighted in color
+                st.write(f'Score: **{ans["score_y"]:.3f}**') # Confidence score
+                st.write(f'Source: {ans["doi_link"]}') # Link to research paper
+                st.markdown("---")
 
-        with socmed:
+        with socmed: # One column for answers from social media (reddit)
             st.subheader("Social Media")
+            for ans in ans_socmed:
+                ans_index_start = ans['content'].find(ans['answer'])
+                ans_index_end = ans_index_start + len(ans['answer'])
+                st.markdown(ans['content'][:ans_index_start] + "**<span style='color:#02f53f'>" + ans['content'][ans_index_start:ans_index_end] + "</span>**" + ans['content'][ans_index_end:], unsafe_allow_html=True) # Answer highlighted in color
+                st.write(f'Score: **{ans["score_y"]:.3f}**') # Confidence score
+                st.write(f'Source: {ans["thread_link"]}') # Link to reddit post
+                st.markdown("---")
 
 
 if __name__ == '__main__':
