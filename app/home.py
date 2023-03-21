4050aec08cae5899eafe74282cc40734553b7ae1
textomatic <102237745+textomatic@users.noreply.github.com>
2023-03-21 01:27:50 -0400

    Update Streamlit page

--- app/home.py
+++ app/home.py
@@ -12,7 +12,7 @@ def main():
         submit = st.form_submit_button('Ask!') # Submit button
 
     if submit and query is not None:
-
+    
         response = requests.get('http://35.207.46.114:8060/search/'+query, headers={"api-key": st.secrets["haystack_api_key"]}) # API endpoint for nutritionbot
         response_json = response.json() # Get json from response
         ans_respp = response_json['research_paper_results']
@@ -21,24 +21,34 @@ def main():
         respp, socmed = st.columns([2, 1])
 
         with respp: # One column for answers from research paper
-            st.subheader("Research Papers") 
-            for ans in ans_respp:
-                ans_index_start = ans['content'].find(ans['answer'])
-                ans_index_end = ans_index_start + len(ans['answer'])
-                st.markdown(ans['content'][:ans_index_start] + "**<span style='color:#02f53f'>" + ans['content'][ans_index_start:ans_index_end] + "</span>**" + ans['content'][ans_index_end:], unsafe_allow_html=True) # Answer highlighted in color
-                st.write(f'Score: **{ans["score_y"]:.3f}**') # Confidence score
-                st.write(f'Source: {ans["doi_link"]}') # Link to research paper
+            st.subheader("Research Papers")
+            if len(ans_respp) == 0: # It is possible that results may be empty due to low confidence scores
+                st.markdown(" ")
+                st.markdown("#### No results!")
                 st.markdown("---")
+            else:
+                for ans in ans_respp:
+                    ans_index_start = ans['content'].find(ans['answer'])
+                    ans_index_end = ans_index_start + len(ans['answer'])
+                    st.markdown(ans['content'][:ans_index_start] + "**<span style='color:#02f53f'>" + ans['content'][ans_index_start:ans_index_end] + "</span>**" + ans['content'][ans_index_end:], unsafe_allow_html=True) # Answer highlighted in color
+                    st.write(f'Score: **{ans["score_y"]:.3f}**') # Confidence score
+                    st.write(f'Source: {ans["doi_link"]}') # Link to research paper
+                    st.markdown("---")
 
         with socmed: # One column for answers from social media (reddit)
             st.subheader("Social Media")
-            for ans in ans_socmed:
-                ans_index_start = ans['content'].find(ans['answer'])
-                ans_index_end = ans_index_start + len(ans['answer'])
-                st.markdown(ans['content'][:ans_index_start] + "**<span style='color:#02f53f'>" + ans['content'][ans_index_start:ans_index_end] + "</span>**" + ans['content'][ans_index_end:], unsafe_allow_html=True) # Answer highlighted in color
-                st.write(f'Score: **{ans["score_y"]:.3f}**') # Confidence score
-                st.write(f'Source: {ans["thread_link"]}') # Link to reddit post
+            if len(ans_socmed) == 0: # It is possible that results may be empty due to low confidence scores
+                st.markdown(" ")
+                st.markdown("#### No results!")
                 st.markdown("---")
+            else:
+                for ans in ans_socmed:
+                    ans_index_start = ans['content'].find(ans['answer'])
+                    ans_index_end = ans_index_start + len(ans['answer'])
+                    st.markdown(ans['content'][:ans_index_start] + "**<span style='color:#02f53f'>" + ans['content'][ans_index_start:ans_index_end] + "</span>**" + ans['content'][ans_index_end:], unsafe_allow_html=True) # Answer highlighted in color
+                    st.write(f'Score: **{ans["score_y"]:.3f}**') # Confidence score
+                    st.write(f'Source: {ans["thread_link"]}') # Link to reddit post
+                    st.markdown("---")
 
 
 if __name__ == '__main__':
